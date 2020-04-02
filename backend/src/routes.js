499fba8888cf87fa06b22fe754e874383f4d8181
Wanner Menezes <owannerm@gmail.com>
2020-04-01 22:34:50 -0300

    Validations and Tests

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const {celebrate, Segments, Joi} = require('celebrate')
 
 const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/IncidentController')
@@ -8,29 +9,42 @@ const SessionController = require('./controllers/SessionController')
 const routes = express.Router()
 
 //login
-routes.post('/sessions', SessionController.create)
-
+routes.post('/sessions', SessionController.create) //validar se id está sendo enviado
 
 //listagem de ongs
 routes.get('/ongs', OngController.index)
-
 //cadastro de ongs
-routes.post('/ongs', OngController.create)
-
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}), OngController.create)
 
 //listagem de casos
-routes.get('/incidents', IncidentController.index)
-
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    })
+}),IncidentController.index)
 //cadastro de casos
-routes.post('/incidents', IncidentController.create)
+routes.post('/incidents', IncidentController.create) //validar body e header
 
 //deletar casos
-routes.delete('/incidents/:id', IncidentController.delete)
-
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+       id: Joi.number().required(),
+    })
+}), IncidentController.delete)
 
 //listar caso especifico
-routes.get('/profile', ProfileController.index)
-
-
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+}), ProfileController.index)
 
 module.exports = routes
